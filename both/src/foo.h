b2e3e2add84437ce9c85db67aa0048e863ff3c75
Christopher Sahnwaldt <jcsahnwaldt@gmail.com>
2021-09-05 04:56:54 +0200

    done

--- both/src/foo.h
+++ both/src/foo.h
@@ -1,9 +1,19 @@
 #ifndef FOO_H
 #define FOO_H
 
+#include <stdint.h>
+
+// https://github.com/rust-lang/rust/blob/master/library/core/src/ptr/metadata.rs
+typedef struct VTable {
+  void (*drop_in_place)(void*);
+  uintptr_t size_of;
+  uintptr_t align_of;
+} VTable;
+
 typedef struct Foo Foo;
 
 typedef struct FooFns {
+  VTable metadata;
   void (*foo)(Foo*);
 } FooFns;
 
